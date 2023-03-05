51f039a4d55ba27137a9502ce347b1e11d9ee66f
bhargavipotnuru <123236762+bhargavipotnuru@users.noreply.github.com>
2023-03-05 18:17:46 +0530

    Add files via upload

---
+++ addevent.js
@@ -0,0 +1,13 @@
+console.log("I'm before add event listener");
+addEventListener('click', () => {
+    console.log('You clicked something');
+})
+console.log("I'm after add event listener");console.log("I'm before add event listener");
+addEventListener('click', () => {
+    console.log('You clicked something');
+})
+console.log("I'm after add event listener");
+
+
+
+
